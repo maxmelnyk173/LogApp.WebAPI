c99dfa674235484a3b9bcc5482feb754353eacad
Max_M <omen5865@gmail.com>
2021-07-03 13:19:07 +0300

    updated account controller

--- LogApp/LogApp/Data/ResetPasswordViewModel.cs
+++ LogApp/LogApp/Data/ResetPasswordViewModel.cs
@@ -6,14 +6,9 @@ using System.Threading.Tasks;
 
 namespace LogApp.Data
 {
-    public class ChangePasswordViewModel
+    public class ResetPasswordViewModel
     {
         public string Id { get; set; }
-
-        [Required]
-        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
-        [DataType(DataType.Password)]
-        public string OldPassword { get; set; }
 
         [Required]
         [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
